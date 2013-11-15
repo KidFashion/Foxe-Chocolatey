fb660e9086a2b8361abc4f66b6e36556a48c3df8
KidFashion <scotto.a@gmail.com>
2013-11-15 23:18:13 +0100

    Import of foxe.install chocolatey package

---
+++ foxe.install/tools/chocolateyInstall.ps1
@@ -0,0 +1,2 @@
+﻿$dir = $env:ChocolateyInstall
+Install-ChocolateyPackage 'foxe' 'exe' "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /DIR=$dir\lib\foxe" 'http://www.firstobject.com/foxesetup242.exe'
---
+++ foxe.install/tools/chocolateyUninstall.ps1
@@ -0,0 +1,7 @@
+﻿$uninstaller = ls $env:ChocolateyInstall\lib\foxe\unins*.exe
+if (($uninstaller|measure-object).Count -gt 1)
+{
+$uninstaller = $uninstaller[0]
+}
+
+&$uninstaller /VERYSILENT /SUPPRESSMSGBOXES /NORESTART 
